d63f919e863eaaf4606aca2028460ad2d9900252
Zwartkat <zwartkat2@gmail.com>
2026-01-15 13:26:33 +0100

    chore: remove useless component

--- apps/web/src/pages/DeckBuilderPage.vue
+++ apps/web/src/pages/DeckBuilderPage.vue
@@ -1,17 +1,11 @@
 <template>
-  <DeckBuilderComponent />
-  <hr />
-  <PokemonCardListComponent :pokemon-cards="pokemonCards" />
-  <PokemonCardModalComponent :view="false" />
+
 </template>
 
 <script lang="ts" setup>
 import { onMounted } from 'vue'
-import PokemonCardListComponent from '../components/PokemonCardListComponent.vue'
 import { usePokemonCardStore } from '../stores/pokemonCardStore.ts'
 import { storeToRefs } from 'pinia'
-import PokemonCardModalComponent from '../components/PokemonCardModalComponent.vue'
-import DeckBuilderComponent from '../components/DeckBuilderComponent.vue'
 
 const pokemonCardsStore = usePokemonCardStore()
 const { pokemonCards } = storeToRefs(pokemonCardsStore)
